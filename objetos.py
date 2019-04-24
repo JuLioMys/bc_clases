bcd7d7f94f84128be7ca5219bce699be6b8408be
Julio <juliomystorres@gmail.com>
2019-04-24 18:20:47 -0400

    Ejercicio de agregar edad

--- objetos.py
+++ objetos.py
@@ -2,13 +2,19 @@
 class Dino: # Class es la receta para crear un objeto
     patas= 4   # Patas y nombre son parametros
     nombre = None
-    def __init__(self, canti_patas, un_nombre): # "self" se refiere al objeto
-        self .patas= canti_patas
+    def __init__(self, un_nombre): # "self" se refiere al objeto
         self.nombre =un_nombre
-        print("Soy un dinosaurio",",me llamo", self, nombre, "y tengo", patas,)
+        print("Soy un dinosaurio",",me llamo", self.nombre")
+
     def get_patas(self):
         return self.patas
-    def set_patas(self, cantidad)
+    
+    def set_patas(self, cantidad):
+        self.patas= cantidad
+
+        def cortar_patas (self):
+            self.patas= self.patas -1
+
 pepito = Dino(4, "Pepito")
 
 
@@ -21,4 +27,25 @@ class Persona:
         self.nombre = un_nombre
 
 humano = Persona("Pepito")
+"""
+#Modificar la clase persona, agregarle atributo edad y 
+# un metodo get_edad
+#inicializar/crear un objeto de tipo persona y hacerle complor
+#anhos
+
+class Persona:
+    nombre= None
+    edad= None
+    def __init__(self, el_nombre,la_edad):
+        self.nombre= el_nombre
+        self.edad= la_edad
+        print("Hola! Mi nombre es", self ,nombre,"y tengo", edad, "anhos" )
+    
+    def get_nombre(self, nombre):
+        return self.nombre
+
+    def get_edad(self):
+        self.edad = self.edad +1
+        
+
 

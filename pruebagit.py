9bdbed515880d98e694b8462f771519886145bfa
Julio <juliomystorres@gmail.com>
2019-04-24 16:54:29 -0400

    agregue ejercicio persona

--- pruebagit.py
+++ pruebagit.py
@@ -1,5 +1,10 @@
-#esto es una prueba
-print ("que tal moodo")
-print("chauuuuu")
-print ("algo 2")
-print ("algo3")
+
+#En el archivo persona.py crear una clase Persona con atributo nombre
+#despues instanciar un objeto de tipo persona
+class Persona:
+    nombre = None
+    def __init__(self, un_nombre):
+        self.nombre = un_nombre
+        print("Me llamo",self.nombre)
+
+humano = Persona("Pepito")
